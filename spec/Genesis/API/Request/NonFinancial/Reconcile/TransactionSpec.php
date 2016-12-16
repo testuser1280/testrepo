455a3c4840d281af39c032876ec91bd561a749c8
test user <tretrupred@throya.com>
2016-12-16 09:48:16 +0200

    1

---
+++ spec/Genesis/API/Request/NonFinancial/Reconcile/TransactionSpec.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace spec\Genesis\API\Request\Reconcile;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TransactionSpec extends ObjectBehavior
+{
+    protected $faker;
+
+    function __construct()
+    {
+        $this->faker = \Faker\Factory::create();
+
+        $this->faker->addProvider(new \Faker\Provider\Uuid($this->faker));
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Genesis\API\Request\Reconcile\Transaction');
+    }
+
+    function it_should_validate_with_arn()
+    {
+        $this->setArn($this->faker->uuid);
+        $this->getDocument()->shouldNotBeEmpty();
+        $this->shouldNotThrow('\Genesis\Exceptions\ErrorParameter')->during('getDocument');
+    }
+
+    function it_should_validate_with_transaction_id()
+    {
+        $this->setTransactionId($this->faker->uuid);
+        $this->getDocument()->shouldNotBeEmpty();
+        $this->shouldNotThrow('\Genesis\Exceptions\ErrorParameter')->during('getDocument');
+    }
+
+    function it_should_validate_with_unique_id()
+    {
+        $this->setUniqueId($this->faker->uuid);
+        $this->getDocument()->shouldNotBeEmpty();
+        $this->shouldNotThrow('\Genesis\Exceptions\ErrorParameter')->during('getDocument');
+    }
+
+    function it_should_fail_when_no_parameters()
+    {
+        $this->shouldThrow('\Genesis\Exceptions\ErrorParameter')->during('getDocument');
+    }
+
+    function it_should_fail_when_missing_required_parameters()
+    {
+        $this->setArn(null);
+        $this->setTransactionId(null);
+        $this->setUniqueId(null);
+
+        $this->shouldThrow()->during('getDocument');
+    }
+
+    public function getMatchers()
+    {
+        return array(
+            'beEmpty' => function ($subject) {
+                return empty($subject);
+            },
+        );
+    }
+}
